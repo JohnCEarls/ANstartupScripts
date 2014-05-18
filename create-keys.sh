b39f373adc2efe3eaa0305532b6e0ff4eabbca18
John C. Earls <yt.rabb@gmail.com>
2014-05-18 09:09:27 -0700

    This appears to make the proper crt

--- create-keys.sh
+++ create-keys.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
  
 #Required
 domain=$1
@@ -27,27 +26,15 @@ fi
 echo "Generating key request for $domain"
  
 #Generate a key
-openssl genrsa -des3 -passout pass:$password -out $domain.key 2048 -noout
+openssl genrsa -des3 -passout pass:$password -out $domain.key 1024 -noout
  
 #Remove passphrase from the key. Comment the line out to keep the passphrase
-echo "Removing passphrase from key"
-openssl rsa -in $domain.key -passin pass:$password -out $domain.key
  
 #Create the request
 echo "Creating CSR"
 openssl req -new -key $domain.key -out $domain.csr -passin pass:$password \
     -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 
+echo "Removing passphrase from key"
+openssl rsa -in $domain.key -passin pass:$password -out $domain.key
 openssl x509 -req -days 365 -in $domain.csr -signkey $domain.key -out $domain.crt
-echo "---------------------------"
-echo "-----Below is your CSR-----"
-echo "---------------------------"
-echo
-cat $domain.csr
- 
-echo
-echo "---------------------------"
-echo "-----Below is your Key-----"
-echo "---------------------------"
-echo
-cat $domain.key
